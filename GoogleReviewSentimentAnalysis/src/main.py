249dab5465675fc7a36d7c23478e641e6b89e179
Milad <miladagdam@gmail.com>
2023-12-29 15:48:39 +0330

    adding run scripts for streamlit

--- GoogleReviewSentimentAnalysis/src/main.py
+++ GoogleReviewSentimentAnalysis/src/main.py
@@ -1,3 +1,19 @@
+import nltk
+import pandas as pd
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment import SentimentIntensityAnalyzer
+import emoji
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('output.csv')
+
+# detect rows with missing reviews
+df.isnull().sum()
+
 def clean_text(text):
     # Convert text to lowercase
     text = text.lower()
@@ -32,6 +48,19 @@ def remove_emojis(text):
     cleaned_text = emoji.demojize(text)
     return cleaned_text
 
+positive_words = set(["legend","good", "excellent", "awesome", "amazing", "fantastic", "finest", "fantastic", "quick", "beautifull"])
+
+# Function to adjust sentiment based on positive words
+def adjust_sentiment_based_on_positive_words(text, sentiment_score):
+    # Check if any positive words are present in the text
+    if any(word in text for word in positive_words):
+        # Increase the sentiment score (adjust as needed)
+        sentiment_score += 0.1
+    return sentiment_score
+    # Remove emojis from the text
+    cleaned_text = emoji.demojize(text)
+    return cleaned_text
+
 def preprocess_pipeline(text):
     # Chain your preprocessing functions
     text = clean_text(text)
@@ -48,8 +77,10 @@ df['cleaned_review'] = df['comment'].apply(preprocess_pipeline)
 sia = SentimentIntensityAnalyzer()
 
 # Analyze sentiment and create a new column
-df['sentiment_score'] = df['cleaned_review'].apply(lambda x: sia.polarity_scores(x)['compound'])
+df['sentiment_score'] = df.apply(lambda row: sia.polarity_scores(row['cleaned_review'])['compound'], axis=1)
 
+# Adjust sentiment based on positive words
+df['sentiment_score'] = df.apply(lambda row: adjust_sentiment_based_on_positive_words(row['cleaned_review'], row['sentiment_score']), axis=1)
 # Define a function to label sentiments
 def label_sentiment(score):
     if score >= 0.05:
