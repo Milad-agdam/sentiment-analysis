dbe176c0f085e8652adc9a91ff7cbc2655d8ce81
Milad <miladagdam@gmail.com>
2024-01-01 15:17:32 +0330

    adding tree map

--- GoogleReviewSentimentAnalysis/src/run.py
+++ GoogleReviewSentimentAnalysis/src/run.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import plotly.express as px
 from wordcloud import WordCloud
 
-
+# Define your color palette for the sentiment labels
 colors = ["#7fcdbb", '#432371',"#FAAE7B" ]
 
 st.set_page_config(
@@ -26,20 +26,23 @@ data = pd.read_csv('data.csv')
 col1, col2 = st.columns(2)
 with col1:
     # sentiment_counts = data['sentiment_label'].value_counts()
+    st.subheader('Distribution of Sentiment Labels')
+
+
+    # Create the countplot
+    sns.set_theme(style="whitegrid")
     sentiment_counts = sns.countplot(data, x="sentiment_label", palette=colors)
     st.pyplot(sentiment_counts.get_figure())
+    # Create the wordcount
     st.subheader('Word Cloud for Positive Sentiment')
     positive_comments = data["cleaned_review"][data['sentiment_label']  == "positive"]
     positive_text = " ".join(comment for comment in positive_comments)
     wordcloud = WordCloud(background_color="white", ).generate(positive_text)
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
+    st.pyplot()
 
 with col2:
-    # piechart = data.sentiment_label.value_counts().plot(kind='pie')
-    # plt.axis('equal')
-    # st.pyplot(piechart)
-    # Display percentage of positive, negative and neutral sentiments
     st.subheader('Sentiment Proportion')
     positive = data['sentiment_label'][data['sentiment_label'] == "positive"].count()
     negative = data['sentiment_label'][data['sentiment_label'] == "negative"].count()
@@ -57,6 +60,11 @@ with col2:
     st.pyplot(fig)
     
 # Set subheader for treemap
+
+sentiment_counts = data['sentiment_label'].value_counts().reset_index()
+sentiment_counts.columns = ['sentiment_label', 'counts']
+
+
 st.subheader('Treemap of Sentiment Distribution')
 
 # Creating a treemap using Plotly
